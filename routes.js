f207e47755fe2d73d5afb7965cf8a6c9a5d0b338
satishp21 <satishdpanchal786@gmail.com>
2023-03-03 16:58:00 +0530

    task 9 kickstart nodejs clean code with route 1

---
+++ routes.js
@@ -0,0 +1,44 @@
+const fs=require("fs")
+const path = require("path")
+const requestHandler = (req,res)=> {
+    const url = req.url
+    const method = req.method
+    if (url === '/'){
+        // res.setHeader('Content-Type','text/html')
+        const filepath = path.join(__dirname,"message.txt")
+        fs.readFile(filepath,{encoding:'utf-8'}, (err,data) =>{
+            if (err){
+                console.log(err);
+            }
+            console.log(`data from file` + data);
+            res.write('<html>')
+            res.write('<head><title> Enter message </title></head>')
+            res.write(`<body><h1>${data}</h1></body>`)
+            res.write(`<body><form action ="/message" method="POST"><input type ="text" name="message" /><button  type="submit">Send</button></form></body>`)
+            res.write('</html>')
+            return res.end()
+        })
+    }
+    if (url === '/message' && method === "POST"){
+        // res.setHeader('Content-Type','text/html')
+        const body = []
+        req.on('data',(chunk) => {
+            console.log(chunk);
+            body.push(chunk)
+        });
+        return req.on("end", () =>{
+            const parsedBody=Buffer.concat(body).toString();
+            // console.log(parsedBody)
+            const message = parsedBody.split("=")[1];
+            fs.writeFileSync("message.txt", message)
+            res.statusCode = 302;
+            res.setHeader('Location','/')
+            return res.end()
+        })
+    
+    }
+
+}
+
+module.exports = requestHandler;
+
