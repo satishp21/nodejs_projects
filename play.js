2e746de3919ba58c41c33a7160d05c133d308f54
satishp21 <satishdpanchal786@gmail.com>
2023-03-04 17:55:04 +0530

    task 3/22 express js

--- play.js
+++ play.js
@@ -1,18 +1,23 @@
 // const http = require('http'); // no need as using app.listen(3000)
 
 const express = require('express')
+const bodyParser= require("body-parser")
 const app = express()
 
-app.use((req,res,next) => {
-    console.log("in the middleware!")
-    next()
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(('/add-product'),(req,res,next) => { //before "/" as request goes throgh file top to bottom
+    res.send('<form action="/product" method="POST" ><input type="text" name ="title"><input type="number" name ="size"><button type="submit"> Add Product</buttton></form>')
 })
 
-app.use((req,res,next) => {
-    console.log("in the another middleware!")
-    res.send('<h1>HEllo from express</h1>')
-    // res.send({ key1: 5 })
+app.use('/product', (req,res,next) =>{
+    console.log(req.body)
+    res.redirect('/')
 })
+
+app.use(('/'),(req,res,next) => {
+    res.send('<h1>HEllo from express</h1>')
+})
+
 
 // const server = http.createServer(app) // no need as using app.listen(3000)
 
