ab632fa93fc61cc67259349c4fc3d9990115ef74
satishp21 <satishdpanchal786@gmail.com>
2023-03-08 15:05:53 +0530

    added css in individual html

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,18 @@
 const express = require('express')
 
+const path= require("path")
+
+const rootDir = require('../util/path') //another way to gove path to html 
 const router = express.Router()
 
-router.get(('/add-product'),(req,res,next) => { //before "/" as request goes throgh file top to bottom
-    res.send('<form action="/product" method="POST" ><input type="text" name ="title"><input type="number" name ="size"><button type="submit"> Add Product</buttton></form>')
+router.get(('/add-product'),(req,res,next) => {
+    // res.sendFile(path.join(__dirname,'../','views','add-product.html')) // one way to give path to html
+    // // '../' can be written as '..' or can be used a helper/util
+    res.sendFile(path.join(rootDir,'views','add-product.html'))
 })
 
-router.post('/product', (req,res,next) =>{
+
+router.post('/add-product', (req,res,next) =>{
     console.log(req.body)
     res.redirect('/')
 })
