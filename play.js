0dcdc179a590df7be4880c4265dcff44ed19768c
satishp21 <satishdpanchal786@gmail.com>
2023-03-08 15:09:24 +0530

    css includes in individual html

--- play.js
+++ play.js
@@ -1,4 +1,5 @@
 //created admin route and shop route to make code clean.
+const path = require("path")
 const express = require('express')
 const bodyParser= require("body-parser")
 const app = express()
@@ -7,11 +8,11 @@ const adminRoutes = require('./routes/admin')
 const shoproutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(adminRoutes)
+app.use('/admin', adminRoutes);
 app.use(shoproutes)
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not fount</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
 // const server = http.createServer(app) // no need as using app.listen(3000)
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,15 @@
 const express = require('express')
 
+const path= require("path")
+
+const rootDir = require('../util/path')
+
 const router = express.Router()
 
 router.get(('/'),(req,res,next) => {
-    res.send('<h1>HEllo from express</h1>')
+    // res.sendFile(path.join(__dirname,'../','views','shop.html')) // one way to give path to html
+    // // '../' can be written as '..' or can be used as helper/util
+    res.sendFile(path.join(rootDir,'views','shop.html'))
 })
 
 module.exports = router
