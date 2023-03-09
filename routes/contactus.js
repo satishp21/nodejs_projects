4fadb0b7d7ce5b2d18fff433659fad53eb19c727
satishp21 <satishdpanchal786@gmail.com>
2023-03-09 21:31:58 +0530

    task 7/22 express js

--- routes/contactus.js
+++ routes/contactus.js
@@ -1,14 +1,19 @@
 const express = require('express')
 
 const path= require("path")
-
 const rootDir = require('../util/path')
 
 const router = express.Router()
 
+// import { contactuscontroller } from '../controllers/contactus' //for controllers
+
+
 router.get('/contactus',(req,res,next)=>{
     res.sendFile(path.join(rootDir,'views','contactus.html'))
 })
+
+// router.get('/contactus',contactuscontroller)
+
 
 router.post('/contactus',(req,res,next)=>{
     res.sendFile(path.join(rootDir,'views','success.html'))
