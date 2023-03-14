0791da1b7e056eb87972f426d25ea0485b048371
satishp21 <satishdpanchal786@gmail.com>
2023-03-14 13:49:28 +0530

    task 17/22 express js

--- E-CommerceWebsite/models/product.js
+++ E-CommerceWebsite/models/product.js
@@ -1,48 +1,27 @@
-const db = require('../util/database')
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart');
-const path = require('path')
+const sequelize = require('../util/database');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
+const Product = sequelize.define('product',{
+  id:{
+    type:Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull : false,
+    primaryKey : true
+  },
+  title: Sequelize.STRING,
+  price:{
+    type :Sequelize.DOUBLE,
+    allowNull : false
+  },
+  imageURL:{
+    type:Sequelize.STRING,
+    allowNull: false
+  },
+  description:{
+    type:Sequelize.STRING,
+    allowNull: false
+  },
+})
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    return db.execute('INSERT INTO products (title,price,description,imageURL) VALUES(?, ?, ?, ?)',
-    [this.title,this.price,this.description,this.imageUrl]
-    )
-  }
-
-  static deleteById(id) {
-    return db.execute('DELETE FROM products WHERE products.id = ?',[id]);
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products')
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?',[id]);
-  }
-};
+module.export = Product;
--- E-CommerceWebsite/util/database.js
+++ E-CommerceWebsite/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2")
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host : "localhost",
-    user : "root",
-    database:"node-proj",
-    password:"S@tishpa2001"
+const sequelize = new Sequelize('node-proj','root','S@tishpa2001',{
+    dialect : 'mysql', 
+    host:'localhost'
 })
 
-module.exports = pool.promise();
+module.exports = sequelize;
