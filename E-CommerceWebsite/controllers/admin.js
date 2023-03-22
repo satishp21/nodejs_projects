67b7431ed9e80774748888154d69c3fe9f7e262b
satishp21 <satishdpanchal786@gmail.com>
2023-03-22 10:41:50 +0530

    task 21 of 22 express js

--- E-CommerceWebsite/controllers/admin.js
+++ E-CommerceWebsite/controllers/admin.js
@@ -13,11 +13,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user
+    .createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
+    description: description,
   })
     .then(result => {
       console.log(result);
@@ -35,8 +36,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findAll({where:{id:prodId}})
-    .then(product => {
+  //Product.findByPk(prodId) or yo can do
+  req.user
+  .getProducts({where:{id:prodId}})
+    .then(products => {
+      const product = products[0]
       if (!product) {
         return res.redirect('/');
       }
@@ -44,7 +48,7 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: product[0]
+        product: product
       });
     })
 };
@@ -74,7 +78,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+  .getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
