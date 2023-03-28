d7b510f20ef054c287fc0966f19af96f83180c0c
satishp21 <satishdpanchal786@gmail.com>
2023-03-28 14:33:52 +0530

    task 7/27 razorpay and premium user feature

--- ExpenseApp/util/database.js
+++ ExpenseApp/util/database.js
@@ -1,11 +1,3 @@
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize('expense','root','S@tishpa2001', {
-//     dialect: 'mysql',
-//     host: 'localhost'
-// })
-
-// module.exports = sequelize;
-
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('expense','root','S@tishpa2001', {
     dialect: 'mysql',
--- thisisfolder/util/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('expense','root','S@tishpa2001', {
-    dialect: 'mysql',
-    host: 'localhost'
-})
-
-module.exports = sequelize;
