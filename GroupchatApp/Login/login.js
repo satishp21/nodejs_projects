d292262da6b9f0eafdd5511410932bd4db0eeaff
satishp21 <satishdpanchal786@gmail.com>
2023-04-12 13:49:32 +0530

    Creating Login Screen Frontend 4/17

---
+++ GroupchatApp/Login/login.js
@@ -0,0 +1,26 @@
+function login(e) {
+    e.preventDefault();
+
+    const loginDetails = {
+        email: e.target.email.value,
+        password: e.target.password.value
+    }
+    console.log(loginDetails, "this is login details")
+
+    axios.post('http://localhost:3000/user/login',loginDetails).then(response => {
+        if(response.status === 200 ){
+            console.log(response)
+            console.log(response.data,response.data.user, "this is response.data.user")
+            // localStorage.setItem('token', response.data.token);
+            // window.location.href = "../ExpenseTracker/index.html" // change the page on successful login
+        } else {
+            throw new Error('Failed to login')
+        }
+    }).catch(err => {
+        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
+    })
+}
+
+function forgotpassword() {
+    window.location.href = "../ForgotPassword/index.html"
+}
